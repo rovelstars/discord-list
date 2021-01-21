d0cb506de91275911968b6c41079dd6cfa25ff87
sayantan300 <rovelstars300@gmail.com>
2021-01-21 22:39:50 +0530

    fixed i think

--- src/bot/index.js
+++ src/bot/index.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
-
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const prefix = process.env.PREFIX;
+const commandFiles = fs.readdirSync(__dirname+'/commands').filter(file => file.endsWith('.js'));
 let i = 0;
 let j = commandFiles.length;
 for (const file of commandFiles) {
