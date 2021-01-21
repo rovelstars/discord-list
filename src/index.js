5865b6d3cbaed21220fb736beb66cca76b8299fb
sayantan300 <rovelstars300@gmail.com>
2021-01-21 17:56:26 +0530

    ok

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-require('rovel.js').env.config();
+const rovel = require("rovel.js");
+if(process.args.includes("--nocolors")){
+rovel.text.enabled = false;
+}
+rovel.env.config();
 const mongoose = require('mongoose');
+const Discord = require("discord.js");
+(async () => {
+ let client = new Discord.client();
+ 
+ client.once("ready", () => {
+  console.log("[BOT] online"+` as ${client.user.username+"#"+client.user.tag}`);
+  client.user.setPresence({ activity: { name: 'Watching RDL' }, status: 'idle' });
+ })
+ 
 mongoose.connect(process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
+
 db.on('error', console.error.bind(console, 'connection error:'));
+
 db.once('open', function() {
-  console.log("we're connected! to "+process.env.DB);
+  console.log("[DB] We're connected! to "+process.env.DB);
+});
+
+const port = process.env.PORT || 3000;
+client.login(process.env.TOKEN);
 });
