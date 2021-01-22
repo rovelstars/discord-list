e9d007b704e038e7bd63a1471f7e083486f0387e
sayantan300 <rovelstars300@gmail.com>
2021-01-22 11:12:32 +0530

    wtf

--- src/index.js
+++ src/index.js
@@ -2,19 +2,16 @@ require("module-alias/register");
 var rovel = require("rovel.js");
 rovel.env.config();
 const mongoose = require('mongoose');
-(async () => {
-await mongoose.connect(process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 
-await db.on('error', console.error.bind(console, 'connection error:'));
-
-await db.once('open', function() {
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
   console.log("[DB] We're connected to database!");
 });
 const {app, port} = require("@server/app.js");
 const {client} = require("@bot/index.js");
-await client.login(process.env.TOKEN);
-await app.listen(port, () => {
+client.login(process.env.TOKEN);
+app.listen(port, () => {
  console.log(`[SERVER] Started on port:${port}`);
-})
 });
