218870c6f90b77cddaa0780636f7b96d9f69e6a6
sayantan300 <rovelstars300@gmail.com>
2021-01-29 13:30:47 +0530

    oof

--- src/server/routes/bots.js
+++ src/server/routes/bots.js
@@ -1,33 +1,17 @@
 let Bots = require("@models/bots.js");
 let {client} = require("@bot/index.js");
 let router = require("express").Router;
-router.post("/new", (req, res)=>{
+router.ger("/new", (req, res)=>{
  const bot = new Bots({
-  _id: req.body.id,
- owners: req.body.owners,
- short: req.body.short,
- desc: req.body.desc,
- verified: false,
- support: req.body.support,
- bg: req.body.bg,
- github: req.body.github,
- website: req.body.website,
- donate: req.body.donate,
- invite: req.body.invite,
- servers: undefined,
- ramUsed: undefined,
- ramLeft: undefined,
- msgGot: undefined,
- cmdGot: undefined,
- msgSent: undefined,
- promoted: false,
- votes: 0
+  _id: 603213294265958400,
+ owners: [602902050677981224],
+ short: "works",
+ desc: "hmm testing"
  });
  
  bot.save((err, bot)=>{
-  const msg = err? `{status: 500, msg: ${err}` : `{status: 200, msg: "Bot Added"`;
-  res.send(msg);
-  client.channels.cache.get("804250610571673600").send(`**New Bot Added!**\nBot: <@!${bot._id}>`);
+  if(err) throw err;
+  client.channels.cache.get("804250610571673600").send(`**New Bot Added!**\nBot: <@!${bot._id}>\nOwner: <@!${bot.owners[0]}>`);
  })
 });
 module.exports = router;
