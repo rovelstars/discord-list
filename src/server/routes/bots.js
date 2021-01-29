0168e700b7ca71156ece3c763ba1a1c31611d4ba
sayantan300 <rovelstars300@gmail.com>
2021-01-29 13:35:36 +0530

    mf

--- src/server/routes/bots.js
+++ src/server/routes/bots.js
@@ -1,8 +1,8 @@
 let Bots = require("@models/bots.js");
 let {client} = require("@bot/index.js");
 let router = require("express").Router();
-router.get("/new", (req, res)=>{
- const bot = new Bots({
+router.post("/new", (req, res)=>{
+ /*const bot = new Bots({
   _id: 603213294265958400,
  owners: [602902050677981224],
  short: "works",
@@ -12,6 +12,7 @@ router.get("/new", (req, res)=>{
  bot.save((err, bot)=>{
   if(err) throw err;
   client.channels.cache.get("804250610571673600").send(`**New Bot Added!**\nBot: <@!${bot._id}>\nOwner: <@!${bot.owners[0]}>`);
- })
+ })*/
+ res.send("hmm");
 });
 module.exports = router;
