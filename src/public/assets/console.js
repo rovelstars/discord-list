d4f224befe111c6349dc73e7967fe2d58a68a51a
sayantan300 <rovelstars300@gmail.com>
2021-02-04 22:50:16 +0530

    npm v7, rovel.js v1.1.9 and bug fix

--- src/public/assets/console.js
+++ src/public/assets/console.js
@@ -1,4 +1,4 @@
-;(function () {
+(function () {
     var src = '//cdn.jsdelivr.net/npm/eruda';
     if (!/debug=true/.test(window.location) && localStorage.getItem('active-eruda') != 'true') return;
     document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
