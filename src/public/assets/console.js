f15dbb1367afd983e97f1b46efec7d3aec7aed65
sayantan300 <rovelstars300@gmail.com>
2021-02-04 21:01:29 +0530

    compiled

--- src/public/assets/console.js
+++ src/public/assets/console.js
@@ -1,19 +1,6 @@
-function getSearchParameters() {
-    var prmstr = window.location.search.substr(1);
-    return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
-}
-
-function transformToAssocArray( prmstr ) {
-    var params = {};
-    var prmarr = prmstr.split("&");
-    for ( var i = 0; i < prmarr.length; i++) {
-        var tmparr = prmarr[i].split("=");
-        params[tmparr[0]] = tmparr[1];
-    }
-    return params;
-}
-
-var params = getSearchParameters();
-if(params.console==true){
- (function () { var script = document.createElement('script'); script.src="//cdn.jsdelivr.net/npm/eruda"; document.body.appendChild(script); script.onload = function () { eruda.init() } })();
-}
+;(function () {
+    var src = '//cdn.jsdelivr.net/npm/eruda';
+    if (!/debug=true/.test(window.location) && localStorage.getItem('active-eruda') != 'true') return;
+    document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
+    document.write('<scr' + 'ipt>eruda.init();</scr' + 'ipt>');
+})();
