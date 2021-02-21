b74b2189fe64c041bb737d41e84a94efc5d18244
sayantan300 <rovelstars300@gmail.com>
2021-02-21 15:14:26 +0530

    hmm

--- src/server/app.js
+++ src/server/app.js
@@ -60,15 +60,11 @@ log("[SERVER] Started!\n[SERVER] Webhooks started!");
 app.use('/assets', express.static(path.resolve("src/public/assets")));
 app.use('/bots', bots);
 app.get("/", async (req, res) => {
- try{
- let key = await req.cookies['key'];
+ /*let key = await req.cookies['key'];
  var user = await auth.getUser(key);
  await console.log(user._username);
- await res.render('index.ejs', {user: user});
- }
- catch{
-  res.sendFile(path.resolve("src/public/assets/index.html"));
- }
+ await res.render('index.ejs', {user: user});*/
+ console.log(req.cookies);
 });
 app.get("/favicon.ico", (req, res) => {
  res.redirect("/assets/favicon.ico");
