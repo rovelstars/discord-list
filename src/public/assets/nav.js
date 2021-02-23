5c6d8a063fece4cf34e89fe742d4f54f6ab69afd
sayantan300 <rovelstars300@gmail.com>
2021-02-23 10:33:40 +0530

    new fonts

--- src/public/assets/nav.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.nav-menu');
-
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active')
-})
