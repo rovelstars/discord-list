6f0a724c33b5856a018b3efabf1b77a70fa1843a
sayantan300 <rovelstars300@gmail.com>
2021-03-04 22:45:45 +0530

    fix website?

--- src/utils/auth.js
+++
@@ -1,7 +0,0 @@
-(async()=>{
-const OAuthClient = await require('disco-oauth');
-const auth = await new OAuthClient(process.env.ID, process.env.SECRET);
-auth.scopes = ['identify', 'guilds'];
-auth.redirectURI = "https://bots.rovelstars.ga/auth";
-module.exports = auth;
-});
