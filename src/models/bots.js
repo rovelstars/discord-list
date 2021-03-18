1c57d41bcaef2402d8b54e87f9cd9969da034089
sayantan300 <rovelstars300@gmail.com>
2021-03-18 21:20:33 +0530

    db

--- src/models/bots.js
+++ src/models/bots.js
@@ -1,12 +1,21 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const Bots = new Schema({
- _id: String , //botId
+ _id: {
+    default: () => new Date(),
+    type: Date
+  }, //added at
+ id: {
+  type: String , 
+  unique: true,
+  required: true
+  }, //botId
  owners: [{id: String}], //owners
  short: String, //short desc
  desc: String, //description
  prefix: String, //bot prefix
  verified: { type: Boolean, default: false }, //verified bot or not
+ added: {type: Boolean, default: false},
  support: String, //support server id
  bg: String, // background image link
  github: String, //github link
@@ -32,13 +41,10 @@ const Bots = new Schema({
 });
 Bots.set('toObject', {
   transform: function (doc, ret) {
-    ret.id = ret._id
     ret.owners.forEach(delid);
     function delid(owner){
      delete owner._id;
     }
-    delete ret._id;
-    delete ret.__v;
   }
 })
 console.log("[DB] Compiling Schema into Model - Bots");
