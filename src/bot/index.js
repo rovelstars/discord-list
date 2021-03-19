62e7fb3961a033ef6369aaf9c737164ddd188828
sayantan300 <rovelstars300@gmail.com>
2021-03-19 13:57:04 +0530

    ok

--- src/bot/index.js
+++ src/bot/index.js
@@ -126,18 +126,10 @@ router.get("/", (req, res) => {
 router.get("/id", (req, res) => {
  res.json({ id: client.user.id });
 });
-router.get("/mainserver", (req, res)=>{
- const guild = client.guilds.cache.get("602906543356379156");
- res.json(guild);
-});
-router.get("/mainserver/members", (req, res)=>{
- const guild = client.guilds.cache.get("602906543356379156");
- res.json({members: guild.members});
-});
 router.get("/mainserver/members/:id", (req, res)=>{
  const user = client.guilds.cache.get("602906543356379156").members.cache.get(req.params.id).user;
  const condition = (user.id)? true : false;
- res.json(condition);
+ res.json({condition});
 });
 router.get("/users/:id", (req, res)=>{
  const user = client.users.cache.get(req.params.id);
