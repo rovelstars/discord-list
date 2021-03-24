188283d861bd832c558d0de16036ceae5645ca09
sayantan300 <rovelstars300@gmail.com>
2021-03-24 07:31:46 +0530

    debug starts

--- src/bot/index.js
+++ src/bot/index.js
@@ -145,8 +145,9 @@ router.get("/bannedusers", (req, res)=>{
 router.get("/bannedusers/:id", (req, res)=>{
  client.guilds.cache.get("602906543356379156").fetchBans().then(list=>{
   let ban = list.map(user=>user.id);
-  if(ban.includes(req.params.id)) return res.json({banned: true});
-  else return res.json({banned: false});
+  console.log(ban);
+  if(ban.includes(req.params.id)) res.json({banned: true});
+  else res.json({banned: false});
  })
 });
 
