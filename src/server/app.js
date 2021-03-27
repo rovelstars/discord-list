9fb3a417b2802808fb684c331c36260539a8067d
sayantan300 <rovelstars300@gmail.com>
2021-03-27 16:45:24 +0530

    Beth

--- src/server/app.js
+++ src/server/app.js
@@ -110,7 +110,7 @@ app.get('/api/*', (req, res)=>{
 })
 
 app.get("/", async (req, res) => {
- fetch(`${process.env.DOMAIN}/api/bots`).then(r=>r.json()).then(d=>{
+ fetch(`${process.env.DOMAIN}/api/bots`).then(r=>r.json()).then(async d=>{
  if(req.cookies['key']){
  var user = req.user;
  await res.render('index.ejs', {user, Allbots});
