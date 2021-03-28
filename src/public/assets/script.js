c790d4eb47ed0590e28014a4a7c4fbb84b7a84ee
sayantan300 <rovelstars300@gmail.com>
2021-03-28 10:43:06 +0530

    aw shit

--- src/public/assets/script.js
+++ src/public/assets/script.js
@@ -2,7 +2,7 @@
   $(window).load(function() {
     // will first fade out the loading animation
     $("#object").fadeOut();
-    $("iframe").fadeOut();
+    $("iframe").delay(1000).fadeOut();
     // will fade out the whole DIV that covers the website.
     $("#loading").delay(100).fadeOut("slow");
     
