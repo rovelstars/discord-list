c8775fc11a2ae29080371fb07cc0997d79ad67ea
sayantan300 <rovelstars300@gmail.com>
2021-04-03 11:36:33 +0530

    iframes are sandboxed now 😎

--- src/public/assets/script.js
+++ src/public/assets/script.js
@@ -1,7 +1,10 @@
 $(window).load(function() {
  // will first fade out the loading animation
  $("#object").fadeOut();
- $("iframe").delay(1000).fadeOut();
  // will fade out the whole DIV that covers the website.
  $("#loading").delay(100).fadeOut("slow");
+ var frames = document.getElementsByTagName('iframe');
+for (var frame of frames) {
+    frame.setAttribute("sandbox", "allow-forms allow-popups");
+}
 });
