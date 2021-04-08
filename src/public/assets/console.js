6c0d61b7b640288f75d9d78d0be567a0302c929c
sayantan300 <rovelstars300@gmail.com>
2021-04-08 15:16:02 +0530

    check the new loader!

--- src/public/assets/console.js
+++ src/public/assets/console.js
@@ -1,6 +1,6 @@
 (function () {
-    var src = '//cdn.jsdelivr.net/npm/eruda';
+    var src = 'cdn.jsdelivr.net/npm/eruda';
     if (!/debug=true/.test(window.location) && localStorage.getItem('active-eruda') != 'true') return;
-    document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
+    document.write('<scr' + 'ipt src=//"' + src + '"></scr' + 'ipt>');
     document.write('<scr' + 'ipt>eruda.init();</scr' + 'ipt>');
 })();
--- src/public/assets/script.js
+++ src/public/assets/script.js
@@ -1,8 +1,7 @@
 $(window).load(function() {
- // will first fade out the loading animation
  $("#object").fadeOut();
- // will fade out the whole DIV that covers the website.
- $("#loading").delay(100).fadeOut("slow");
+ $("#loadingtext").fadeOut();
+ $("#loadingimage").delay(100).fadeOut("slow");
  var frames = document.getElementsByTagName('iframe');
 for (var frame of frames) {
     frame.setAttribute("sandbox", "allow-forms allow-popups");
