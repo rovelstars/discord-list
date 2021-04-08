496e4d81fb9078e6e9d86e2878da4db9a2fbf706
sayantan300 <rovelstars300@gmail.com>
2021-04-08 15:46:11 +0530

    ah

--- src/public/assets/script.js
+++ src/public/assets/script.js
@@ -1,7 +1,6 @@
 $(window).load(function() {
  $("#object").fadeOut();
- $("#loadingtext").fadeOut();
- $("#loadingimage").delay(100).fadeOut("slow");
+ $("#loading").delay(100).fadeOut("slow");
  var frames = document.getElementsByTagName('iframe');
 for (var frame of frames) {
     frame.setAttribute("sandbox", "allow-forms allow-popups");
