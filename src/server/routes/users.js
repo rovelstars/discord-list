05423246d569c7cb70499cd30c56867832c05918
sayantan300 <rovelstars300@gmail.com>
2021-04-09 14:09:18 +0530

    /all will show dscrd.info api!

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -19,6 +19,54 @@ router.get("/", (req, res) => {
   })
  }
 });
+router.get("/all", async (req, res)=>{
+ if(req.query.q){
+  await fetch(`https://api.dscrd.info/search/${req.query.q}`).then(r=>r.json()).then(async resp=>{
+   if(resp.success){
+    if(resp.users.length==0){//need to perform id fetch
+     await fetch(`https://api.dscrd.info/id/${req.query.q}`).then(r=>r.json()).then(async resp=>{
+      if(res.success){
+       const hash = res.user.avatar_url.replace(`https://cdn.discordapp.com/avatars/${data.username}/`, "");
+       hash = hash.replace("?size=1024", "");
+       var users = [{
+        id: resp.user.user_id,
+        username: res.user.username,
+        avatarURL: res.user.avatar_url,
+        avatar: hash,
+        discriminator: res.user.discriminator,
+        tag: res.user.username+"#"+res.user.discriminator
+      }];
+      res.json(users);
+      } else {
+       return res.json({err: "not_found"});
+      }
+     });
+    }
+    else {
+    var users = [];
+    var temp;
+    const members = resp.members;
+    await members.forEach((data, index)=>{
+     const hash = data.avatar_url.replace(`https://cdn.discordapp.com/avatars/${data.username}/`, "");
+     hash = hash.replace("?size=1024", "");
+     temp = {
+      id: data.user_id,
+      username: data.username,
+      avatarURL: data.avatar_url,
+      avatar: hash,
+      discriminator: data.discriminator,
+      tag: data.username+"#"+data.discriminator
+     };
+     users.push(temp);
+    });
+    await res.json(users);
+   }
+   }
+   else return res.json({err: "failed"});
+  })
+ }
+ else return res.json({err: "no_query"});
+});
 router.get("/:id", (req, res) => {
  Users.findOne({ id: req.params.id }).then(user => {
   res.json(user);
