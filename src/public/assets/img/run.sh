36fe7042f6dbf602b7fbe7bbdc20d030fc43b9bf
sayantan300 <rovelstars300@gmail.com>
2021-04-10 10:45:13 +0530

    resize logo for manifest :flushed:

---
+++ src/public/assets/img/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+resize-img logo-512.png --width 36 > logo-36.png
+echo "36x36 done!"
+resize-img logo-512.png --width 48 > logo-48.png
+echo "48x48 done!"
+resize-img logo-512.png --width 72 > logo-72.png
+echo "72x72 done!"
+resize-img logo-512.png --width 96 > logo-96.png
+echo "96x96 done!"
+resize-img logo-512.png --width 144 > logo-144.png
+echo "144x144 done!"
