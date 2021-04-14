af456b1d84b4dc06fa454fb9e24681ebb9e9dc22
sayantan300 <rovelstars300@gmail.com>
2021-04-14 09:20:32 +0530

    use geoip-litd

--- src/server/app.js
+++ src/server/app.js
@@ -1,7 +1,7 @@
 const port = process.env.PORT || 3000;
 const actuator = require('express-actuator');
 const marked = require("marked");
-const geoip = require("iplocation");
+const geoip = require("geoip-lite");
 var cloudflare = require('cloudflare-express');
 var Bots = require("@models/bots.js");
 const users = require("@routes/users.js");
@@ -102,8 +102,8 @@ app.use(checkBanned);
 var weblog = async function(req, res, next) {
  const weburl = process.env.WEBHOOK;
  const user = (req.user)?req.user.tag:"Not logined";
- const geo = await geoip(req.cf_ip);
- const logweb = `**New Log!**\n**Time:** \`${dayjs().format("ss | mm | hh A - DD/MM/YYYY Z")}\`\n**IP:** ||${req.cf_ip}||\n**Path requested:** \`${req.originalUrl}\`\n**Request type:** \`${req.method}\`\n**Location:** ${geo.region.name}\nUser: ${user}`;
+ const geo = await geoip.lookup(req.cf_ip);
+ const logweb = `**New Log!**\n**Time:** \`${dayjs().format("ss | mm | hh A - DD/MM/YYYY Z")}\`\n**IP:** ||${req.cf_ip}||\n**Path requested:** \`${req.originalUrl}\`\n**Request type:** \`${req.method}\`\n**Location:** ${geo.timezone}\nUser: ${user}`;
  await fetch(weburl, {
   method: "POST",
   headers: {
