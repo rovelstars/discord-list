e8b00b172559fcf81d7ceb4c47f0d592347c35b9
sayantan300 <rovelstars300@gmail.com>
2021-04-16 11:08:34 +0530

    ()

--- src/bot/commands/bal.js
+++ src/bot/commands/bal.js
@@ -1,4 +1,4 @@
-if(!args[0]){
+if(args.length===0){
   Users.findOne({id: message.author.id}).then(user=>{
     if(!user) message.reply("Please login to get an account on RDL!\nLogin link:\nhttps://discord.rovelstars.com/login");
     else {
@@ -7,7 +7,7 @@ if(!args[0]){
   })
 }
 else {
- (async=>{
+ (async()=>{
     const userhmm = await getMention(args[0]);
     await Users.findOne({id: userhmm.id}).then(user=>{
       if(!user) message.reply("It seems as if "+userhmm.tag+" never logined on RDL...");
