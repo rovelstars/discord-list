e27139d300669b0a53b65607eba6da9e609e42ee
sayantan300 <rovelstars300@gmail.com>
2021-04-17 20:46:59 +0530

    api and assets should go online when started.

--- src/server/app.js
+++ src/server/app.js
@@ -50,9 +50,8 @@ app.use(latency({header: "ping"}));
 app.use(actuator({basePath:"/api"}));
 var booting = function(req, res, next){
  if(process.uptime()< 10){
-  if(req.path.includes("/assets/")) next();
-  
-  res.sendFile(path.resolve("src/public/assets/loading.html"));
+  if(req.originalUrl.startsWith("/assets")||req.originalUrl.startsWith("/api")) next();
+  else res.sendFile(path.resolve("src/public/assets/loading.html"));
  }
  else next();
 }
