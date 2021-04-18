242d24df5cb1e7c5aa3d5a0270b5e303fc008661
sayantan300 <rovelstars300@gmail.com>
2021-04-18 16:24:22 +0530

    oof

--- src/bot/events/ready.js
+++ src/bot/events/ready.js
@@ -2,7 +2,7 @@ client.once('ready', () => {
  console.log(`[BOT] Logined as ${client.user.tag}`);
  setTimeout(() => {
   client.guilds.cache.get("602906543356379156").channels.cache.get("775231877433917440").send(`>>> Rovel Discord List has Started!\nWith ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
- }, 1000);
+ }, 2000);
  activities_list = [
   "with all the bots on RDL",
   "with the members on RDL",
