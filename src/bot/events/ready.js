7a942f37a1d154b2bbea498ebf98692946c84e26
sayantan300 <rovelstars300@gmail.com>
2021-04-18 14:04:31 +0530

    send log after dying

--- src/bot/events/ready.js
+++ src/bot/events/ready.js
@@ -1,6 +1,8 @@
 client.once('ready', () => {
  console.log(`[BOT] Logined as ${client.user.tag}`);
- client.guilds.cache.get("602906543356379156").channels.cache.get("775231877433917440").send(`>>> Rovel Discord List has Started!\nWith ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
+ setTimeout(() => {
+  client.guilds.cache.get("602906543356379156").channels.cache.get("775231877433917440").send(`>>> Rovel Discord List has Started!\nWith ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
+ }, 1000);
  activities_list = [
   "with all the bots on RDL",
   "with the members on RDL",
