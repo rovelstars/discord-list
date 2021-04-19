22748b60a73b2d5007078a6cf29d2f9e906593f6
sayantan300 <rovelstars300@gmail.com>
2021-04-19 19:01:22 +0530

    wat do i want

--- src/bot/events/message.js
+++ src/bot/events/message.js
@@ -2,11 +2,13 @@ client.on("message", message=>{
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 	let args = message.content.slice(prefix.length).trim().split(/ +/);
 	let command = args.shift().toLowerCase();
+	if(command=""|| !command) message.reply("What do you want?");
+	else{
 	let cmd = searchCommand(command);
 	try{
 	if(!cmd) return message.reply("That command Doesn't exist!");
 	else eval(cmd.code);
 	} catch(e){
 	  message.reply(`An Error Occured!\n\`\`\`\n${e}\n\`\`\``)
-	}
+	}}
 });
