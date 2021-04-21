600ca20f900422b77de7f2ff4c6122c94da48562
sayantan300 <rovelstars300@gmail.com>
2021-04-21 12:17:51 +0530

    delete old commands when reloading

--- src/bot/index.js
+++ src/bot/index.js
@@ -33,6 +33,7 @@ function searchCommand(name){
 }
 
 function reload(){
+ client.commands = [];
 var commandFiles = fs.readdirSync(__dirname + '/commands').filter(file => file.endsWith('.js'));
 let ci = 0;
 let cj = commandFiles.length;
