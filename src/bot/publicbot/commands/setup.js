9eef4fee4f5d4b62aaa87843893afa99ee03d6c5
sayantan300 <rovelstars300@gmail.com>
2021-04-23 13:04:25 +0530

    fixed

--- src/bot/publicbot/commands/setup.js
+++ src/bot/publicbot/commands/setup.js
@@ -1,28 +1,26 @@
-if(message.member.hasPermission("ADMINISTRATOR")){
- Servers.findOne({id: message.guild.id}).then(server=>{
-  if(!server){
- message.channel.send("Kantan sugiru! Im posting this server on RDL!");
- let code;
- message.channel.send("Making an invite link!").then(msg=>{
-  msg.channel.createInvite({maxAge: 0, reason: `${message.author.tag} asked to add this server to RDL!`}).then(invite=>{
-   msg.edit(`Invite code: **${invite.code}**`);
-   code = invite.code;
-  })
- })
- let Server = new Servers({
-  id: message.guild.id,
-  invite: code,
-  owner: message.guild.owner.id
- }).save((err, ser)=>{
-  if(err) message.reply("Nani?! An Error Occurred!\nI cannot add your server to RDL!\n"+err);
-   message.channel.send("Successfully Added your server to RDL!\nPlease update the description of your server on the dashboard on RDL.");
- })
+if (message.member.hasPermission("ADMINISTRATOR")) {
+ Servers.findOne({ id: message.guild.id }).then(server => {
+  if (!server) {
+   message.channel.send("Kantan sugiru! Im posting this server on RDL!");
+   message.channel.send("Making an invite link!").then(msg => {
+    msg.channel.createInvite({ maxAge: 0, reason: `${message.author.tag} asked to add this server to RDL!` }).then(invite => {
+     msg.edit(`Invite code: **${invite.code}**`);
+     let Server = new Servers({
+      id: message.guild.id,
+      invite: invite.code,
+      owner: message.guild.owner.id
+     }).save((err, ser) => {
+      if (err) message.reply("Nani?! An Error Occurred!\nI cannot add your server to RDL!\n" + err);
+      message.channel.send("Successfully Added your server to RDL!\nPlease update the description of your server on the dashboard on RDL.");
+     })
+    })
+   })
   }
   else {
    message.channel.send("Baka! This server is already on RDL!");
   }
  });
 }
-else{
+else {
  message.channel.send("Nani?! You're not an administrator to run this command!");
 }
