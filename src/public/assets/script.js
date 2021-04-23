c57b35d3b0d19bfa6bf7502088c1f2dd4fe1ecf3
sayantan300 <rovelstars300@gmail.com>
2021-04-23 22:19:24 +0530

    load fast!

--- src/public/assets/script.js
+++ src/public/assets/script.js
@@ -1,6 +1,6 @@
 $(window).load(function() {
  $("#object").fadeOut();
- $("#loading").delay(100).fadeOut("slow");
+ $("#loading").fadeOut();
  var frames = document.getElementsByTagName('iframe');
 for (var frame of frames) {
     frame.setAttribute("sandbox", "allow-forms allow-popups");
