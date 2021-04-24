a705790b6c9e16d78edf139cad6a1075037c9dc3
sayantan300 <rovelstars300@gmail.com>
2021-04-24 12:03:24 +0530

    new statue page!

--- src/server/app.js
+++ src/server/app.js
@@ -207,9 +207,15 @@ app.get("/dashboard", async (req, res)=>{
   });
  }
 });
+
+app.get("/status", (req, res)=>{
+ res.render('status.ejs',{});
+});
+
 app.get("/loaderio-39a018887f7a2f8e525d19a772e9defe", (req, res)=>{
  res.sendFile(path.resolve("src/public/assets/verify.txt"));
 });
+
 app.get("/favicon.ico", (req, res) => {
  res.redirect("/assets/img/logo.png");
 });
