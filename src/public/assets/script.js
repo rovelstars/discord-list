811d7a821f198830187155e2353b3dbd775d11a3
sayantan300 <rovelstars300@gmail.com>
2021-04-27 21:45:27 +0530

    ah

--- src/public/assets/script.js
+++ src/public/assets/script.js
@@ -6,3 +6,5 @@ for (var frame of frames) {
     frame.setAttribute("sandbox", "allow-forms allow-popups");
 }
 });
+navigator.registerProtocolHandler(
+    'web+rdl', 'https://discord.rovelstars.com', 'Rovel Discord List');
