5bb7f61a863f3c4ed54e3816b412dd1b200330fa
sayantan300 <rovelstars300@gmail.com>
2021-04-28 09:55:26 +0530

    new crashed page!

--- src/public/assets/script.js
+++ src/public/assets/script.js
@@ -6,5 +6,3 @@ for (var frame of frames) {
     frame.setAttribute("sandbox", "allow-forms allow-popups");
 }
 });
-navigator.registerProtocolHandler(
-    'web+rdl', 'https://discord.rovelstars.com', 'Rovel Discord List');
