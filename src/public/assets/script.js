f369f8a83ea95f93e6ef33efb5d99a0bdc7ceb9d
sayantan300 <rovelstars300@gmail.com>
2021-05-05 14:57:57 +0530

    change color of navbar wirh meta tags

--- src/public/assets/script.js
+++ src/public/assets/script.js
@@ -1,15 +1,17 @@
 $(window).load(function() {
-   var theme = document.getElementById("meta-theme");
-    if(theme){
-     theme.style.display="none";
-     const color = theme.innerText;
-     document.querySelector('meta[name="theme-color"]').setAttribute('content',color);
-     document.querySelector('meta[name="theme-color"]').setAttribute('msapplication-navbutton-color',color);
-    }
+ var theme = document.getElementById("meta-theme");
+ if (theme) {
+  theme.style.display = "none";
+  const color = theme.innerText;
+  document.querySelector('meta[name="theme-color"]').setAttribute('content', color);
+  document.querySelector('meta[name="theme-color"]').setAttribute('msapplication-navbutton-color', color);
+  var nav = document.getElementsByClassName("navbar is-fixed-top");
+  nav[0].style.backgroundColor = color;
+ }
  $("#object").fadeOut();
  $("#loading").fadeOut();
  var frames = document.getElementsByTagName('iframe');
-for (var frame of frames) {
-    frame.setAttribute("sandbox", "allow-forms allow-popups");
-}
+ for (var frame of frames) {
+  frame.setAttribute("sandbox", "allow-forms allow-popups");
+ }
 });
