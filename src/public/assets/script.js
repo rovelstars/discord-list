e040fe2f7b59713163bc1c1da612831577607cd3
sayantan300 <rovelstars300@gmail.com>
2021-05-05 18:03:51 +0530

    network off and on detection!

--- src/public/assets/script.js
+++ src/public/assets/script.js
@@ -14,4 +14,33 @@ $(window).load(function() {
  for (var frame of frames) {
   frame.setAttribute("sandbox", "allow-forms allow-popups");
  }
+ var online = true;
+ const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+   toast.addEventListener('mouseenter', Swal.stopTimer)
+   toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+ });
+ setInterval(() => {
+  if (navigator.onLine != online) {
+   if (navigator.onLine == false) {
+    Toast.fire({
+     icon: 'error',
+     title: 'No Network!'
+    });
+   }
+   if(navigator.onLine==true){
+    Toast.fire({
+     icon:'success',
+     title: 'Network Connection Back!'
+    });
+   }
+   online=navigator.onLine;
+  }
+ }, 1000);
 });
