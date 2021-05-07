f17f12a35962407cb11316d8309c589b272e670b
sayantan300 <rovelstars300@gmail.com>
2021-05-07 22:15:58 +0530

    spam async

--- src/bot/commands/pending.js
+++ src/bot/commands/pending.js
@@ -1,11 +1,11 @@
-Bots.find({added: false}).limit(5).then(bots=>{
+Bots.find({added: false}).limit(5).then(async bots=>{
  let msg="Showing the Oldest 5 Bots that are not added to this server:\n";
- for(const bot of bots){
-  fetch(`${process.env.DOMAIN}/api/client/mainserver/${bot.id}`).then(r=>r.json()).then(d=>{
+ for await(const bot of bots){
+  await fetch(`${process.env.DOMAIN}/api/client/mainserver/${bot.id}`).then(r=>r.json()).then(async d=>{
    if(!d.condition){
-    msg+=`<https://discord.com/api/oauth2/authorize?client_id=${bot.id}&permissions=0&scope=bot> - ${bot.tag}\n`
+    msg+= await `<https://discord.com/api/oauth2/authorize?client_id=${bot.id}&permissions=0&scope=bot> - ${bot.tag}\n`
    }
   });
  }
- message.channel.send(msg);
+ await message.channel.send(msg);
 })
