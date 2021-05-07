ca80e659294d3b09c41bbb636618d0ae3a0d8a5a
sayantan300 <rovelstars300@gmail.com>
2021-05-07 18:40:44 +0530

    servers model update

--- src/models/servers.js
+++ src/models/servers.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const Servers = new Schema({
-   id: {type: Number, required: true},
+   id: {type: String, required: true},
    _id: {
     default: () => new Date(),
     type: Date
