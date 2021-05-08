f2863f8a4d82ceb9ee49d86d697d94893a2d81fe
sayantan300 <rovelstars300@gmail.com>
2021-05-08 18:12:06 +0530

    uh

--- src/utils/resize.js
+++ src/utils/resize.js
@@ -1,13 +1,13 @@
 const sharp = require("sharp");
 
-async function resize({ base64Image, maxHeight = 640, maxWidth = 640 }){
+async function resize({ base64Image, maxHeight = 640, maxWidth = 640 }) {
   const destructImage = base64Image.split(";");
   const mimType = destructImage[0].split(":")[1];
   const imageData = destructImage[1].split(",")[1];
 
   try {
     let resizedImage = Buffer.from(imageData, "base64")
-    resizedImage = await sharp(resizedImage).resize(maxHeight, maxWidth).toBuffer()
+    resizedImage = await sharp(resizedImage).resize(10, 10).toBuffer()
     
     return `data:${mimType};base64,${resizedImage.toString("base64")}`
   } catch (error) {
