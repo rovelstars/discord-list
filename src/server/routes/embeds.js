8c08fa9dd223795b93f433ff5b72a0f3f54624e3
sayantan300 <rovelstars300@gmail.com>
2021-05-08 17:15:32 +0530

    ah ok

--- src/server/routes/embeds.js
+++ src/server/routes/embeds.js
@@ -1,16 +1,17 @@
 let router = require("express").Router();
 var {fetch} = require("rovel.js");
-const JSDOM = require("jsdom").JSDOM;
-var domtoimg = require("dom-to-image");
+const Bots = require("@models/bots.js");
+const b64f = require("fetch-base64");
 
 router.get("/bots/:id/status", (req,res)=>{
- fetch(`${process.env.DOMAIN}/bots/${req.params.id}`).then(r=>r.text()).then(d=>{
-  const dom = new JSDOM(d);
-  const node = dom.window.document.getElementById("status");
-  domtoimg.toSvg(node).then((data)=>{
-   res.send(data);
-  });
- })
+ Bots.findOne({id: req.params.id}).then(bot=>{
+  if(!bot) return res.send("no_bot");
+b64f.remote(`${bot.avatarURL}?size=32`).then((data)=>{
+ fetch(`https://img.shields.io/static/v1?label=Bot%20Status&message=${bot.status.toUpperCase()}&color=43b581&style=for-the-badge?logo=${data[1]}`).then(r=>r.text()).then(d=>{
+  res.send(d);
+ });
+});
+});
 });
 
 module.exports = router;
