fd5425516ca04606a09438e4a386448c340289e8
sayantan300 <rovelstars300@gmail.com>
2021-05-08 17:18:35 +0530

    uh

--- src/server/routes/embeds.js
+++ src/server/routes/embeds.js
@@ -6,8 +6,9 @@ const b64f = require("fetch-base64");
 router.get("/bots/:id/status", (req,res)=>{
  Bots.findOne({id: req.params.id}).then(bot=>{
   if(!bot) return res.send("no_bot");
+  if(!bot.status) bot.status = "ONLINE";
 b64f.remote(`${bot.avatarURL}?size=32`).then((data)=>{
- fetch(`https://img.shields.io/static/v1?label=Bot%20Status&message=${bot.status.toUpperCase()}&color=43b581&style=for-the-badge?logo=${data[1]}`).then(r=>r.text()).then(d=>{
+ fetch(`https://img.shields.io/static/v1?label=Bot%20Status&message=${bot.status}&color=43b581&style=for-the-badge?logo=${data[1]}`).then(r=>r.text()).then(d=>{
   res.send(d);
  });
 });
