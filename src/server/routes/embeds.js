0fcd16bb1f973cf3bdfa8e4430074835f2e790e1
sayantan300 <rovelstars300@gmail.com>
2021-05-08 18:26:02 +0530

    ah

--- src/server/routes/embeds.js
+++ src/server/routes/embeds.js
@@ -1,19 +1,16 @@
 let router = require("express").Router();
 var {fetch} = require("rovel.js");
 const Bots = require("@models/bots.js");
-const b64f = require("fetch-base64");
-const resize = require("@utils/resize.js");
 router.get("/bots/:id/status", (req,res)=>{
  Bots.findOne({id: req.params.id}).then(bot=>{
   if(!bot) return res.send("no_bot");
   if(!bot.status) bot.status = "ONLINE";
- b64f.remote(`${bot.avatarURL}?size=16`).then(async (data)=>{
- const img = await resize(data[1]);
- await fetch(`https://img.shields.io/static/v1?label=Bot%20Status&message=${bot.status}&color=43b581&style=for-the-badge?logo=${img}`).then(r=>r.text()).then(d=>{
+  const style = (req.query.style)?req.query.style:"for-the-badge";
+  const color = (req.query.color)?req.query.color:"43b581";
+  const label = (req.query.label)?req.query.label:"Status";
+ await fetch(`https://img.shields.io/static/v1?label=${label}&message=${bot.status}&color=${color}&style=${style}`).then(r=>r.text()).then(d=>{
   res.send(d);
-  console.log(`https://img.shields.io/static/v1?label=Bot%20Status&message=${bot.status}&color=43b581&style=for-the-badge?logo=${data[1]}`);
  });
-});
 });
 });
 
