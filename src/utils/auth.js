b074488e2d2f4fbc08e72a1e959ff8e51c45f9da
sayantan300 <rovelstars300@gmail.com>
2021-05-09 14:32:01 +0530

    whp dafaq asked for guilds.join?

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,5 @@
 const OAuthClient = require('disco-oauth');
 const auth = new OAuthClient(process.env.ID, process.env.SECRET);
-auth.scopes = ['identify', 'guilds.join'];
+auth.scopes = ['identify'];
 auth.redirectURI = `${process.env.DOMAIN}/api/auth`;
 module.exports = auth;
