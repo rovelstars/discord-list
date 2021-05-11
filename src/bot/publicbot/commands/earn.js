dc44c0d06332f4f268d89750c3f378f37a4c9fde
sayantan300 <rovelstars300@gmail.com>
2021-05-11 17:11:07 +0530

    random earn

--- src/bot/publicbot/commands/earn.js
+++ src/bot/publicbot/commands/earn.js
@@ -5,9 +5,10 @@ else {
   Users.findOne({id: message.author.id}).then(user=>{
     if(!user) message.reply("Nani?! You're not logined!\nPlease login to RDL to make an account in order to recieve money!\nLogin link:\nhttps://discord.rovelstars.com/login");
     else {
-      user.bal+=1;
+     const c = Math.floor(Math.random()*10)+1;
+      user.bal+=c;
       user.save();
-      message.channel.send("Nani o matsu?! You recieved R$ 1!\nThat's nothing compared to me-");
+      message.channel.send("Nani o matsu?! You recieved R$ "+c+"!\nThat's nothing compared to me-");
       cooldownearn.add(message.author.id);
   setTimeout(()=>{
     cooldownearn.delete(message.author.id);
