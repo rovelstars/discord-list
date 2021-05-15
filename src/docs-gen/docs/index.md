18888944aa820396d1e26ed07db8c73716a1e2fe
sayantan300 <rovelstars300@gmail.com>
2021-05-15 15:13:06 +0530

    docs generator!

---
+++ src/docs-gen/docs/index.md
@@ -0,0 +1,5 @@
+---
+title: Hello
+slug: home
+---
+<h1>Hello world!</h1>
---
+++ src/docs-gen/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const matter = require('gray-matter');
+const str = fs.readFileSync('example.html', 'utf8');
+
+var Files = fs.readdirSync(__dirname + '/docs').filter(file => file.endsWith('.md'));
+
+let ci = 0;
+let cj = Files.length;
+for (var file of Files) {
+ const text = fs.readFileSync(`${__dirname}/docs/${file}`,'utf8');
+ const build = matter(text);
+ ci += 1;
+ console.log(`[DOCS] Page Generated - ${file} (${ci}/${cj})`);
+ file = file.replace(".md","");
+ fs.writeFileSync(`${__dirname}/build/${file}.json`, text);
+}
