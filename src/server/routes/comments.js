3043c385ee2f91096706fcb19290d4a073f5129a
rovelhub <support@rovelstars.com>
2021-05-20 18:45:43 +0530

    fix the site

--- src/server/routes/comments.js
+++ src/server/routes/comments.js
@@ -1,3 +1,9 @@
 let router = require("express").Router();
 router.use(require("express").json());
 let Bots = require("@models/bots.js");
+
+router.get("/",(req, res)=>{
+ res.send("wip");
+})
+
+module.exports = router;
