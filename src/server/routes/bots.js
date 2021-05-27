c9c7192d02bdb021448434f619089f6c048b1036
sayantan300 <rovelstars300@gmail.com>
2021-05-27 17:25:39 +0530

    fix?

--- src/server/routes/bots.js
+++ src/server/routes/bots.js
@@ -168,18 +168,13 @@ router.post("/evaldb", (req, res) => {
 });
 
 router.get("/:id/sync", (req, res) => {
- res.sent= false;
  Bots.findOne({ id: req.params.id }).then(user => {
   if (!user){
    res.json({ err: "not_found" });
-   res.sent=true;
   }
   else {
    fetch("https://discord.rovelstars.com/api/client/users/" + user.id).then(r => r.json()).then(u => {
-    if ((u.avatar === user.avatar) && (u.username === user.username) && (u.discriminator === user.discriminator)){
-     if(!res.sent){
-     res.json({ err: "same_data" });
-    }}
+    if ((u.avatar === user.avatar) && (u.username === user.username) && (u.discriminator === user.discriminator)) return res.json({ err: "same_data" });
     else {
      if (u.avatar !== user.avatar) {
       user.avatar = u.avatar;
@@ -191,6 +186,7 @@ router.get("/:id/sync", (req, res) => {
       user.discriminator = u.discriminator;
      }
      user.save();
+     res.json({ success: true });
      fetch("https://discord.rovelstars.com/api/client/log", {
       method: "POST",
       headers: {
@@ -207,7 +203,7 @@ router.get("/:id/sync", (req, res) => {
      });
     }
    });
-   res.json({ success: true });
+   
   }
  });
 });
