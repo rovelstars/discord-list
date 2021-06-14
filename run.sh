de0367e67384f5a765b7c0cd3072a0ed010daec8
sayantan300 <rovelstars300@gmail.com>
2021-06-14 18:20:56 +0530

    new build!

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 npm i rdl-i18n
+npm i shelljs -g
 cd src/public/assets/img/bot
 resize-img logo-512.png --width 36 > logo-36.png
 echo "36x36 done!"
@@ -11,3 +12,7 @@ resize-img logo-512.png --width 96 > logo-96.png
 echo "96x96 done!"
 resize-img logo-512.png --width 144 > logo-144.png
 echo "144x144 done!"
+git clone --no-checkout https://github.com/rovelstars/discord-list.git rdl
+mv rdl/.git ./.git
+rm -f rdl
+echo "Git Directory Successfully Copied!"
