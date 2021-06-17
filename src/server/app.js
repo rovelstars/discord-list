6192821ca6758ff7be9236b189475f2f5b89f1cf
sayantan300 <rovelstars300@gmail.com>
2021-06-17 09:56:40 +0530

    revert back to a fixed version.

--- src/server/app.js
+++ src/server/app.js
@@ -88,6 +88,66 @@ var booting = function(req, res, next) {
 }
 app.use(booting);
 
+const extendTimeoutMiddleware = (req, res, next) => {
+  const space = ' ';
+  let isFinished = false;
+  let isDataSent = false;
+  let i = 0;
+
+  // Only extend the timeout for API requests
+  if (!req.url.includes('/api')) {
+    next();
+    return;
+  }
+
+  res.once('finish', () => {
+    isFinished = true;
+  });
+
+  res.once('end', () => {
+    isFinished = true;
+  });
+
+  res.once('close', () => {
+    isFinished = true;
+  });
+
+  res.on('data', (data) => {
+    // Look for something other than our blank space to indicate that real
+    // data is now being sent back to the client.
+    if (data !== space) {
+      isDataSent = true;
+    }
+  });
+
+  const waitAndSend = () => {
+   if(i>10){
+    res.json({err: "timeout"});
+   }
+   else{
+   i++;
+    setTimeout(() => {
+      // If the response hasn't finished and hasn't sent any data back....
+      if (!isFinished && !isDataSent) {
+        // Need to write the status code/headers if they haven't been sent yet.
+        if (!res.headersSent) {
+          res.writeHead(202);
+        }
+
+        res.write(space);
+
+        // Wait another 15 seconds
+        waitAndSend();
+      }
+    }, 15000);
+  waitAndSend();
+  next();
+  }
+  }
+};
+
+app.use(extendTimeoutMiddleware);
+
 var checkBanned = async function(req, res, next) {
  res.locals.req = req;
  res.locals.res = res;
@@ -242,7 +302,6 @@ app.use('/api/preferences', prefers);
 
 app.get('/api', (req, res) => {
  res.json({ hello: "coder!" });
- console.log("test");
 })
 
 app.get('/api/report', (req, res) => {
@@ -298,6 +357,8 @@ app.post("/api/translate", async (req, res) => {
   }
  }
 });
+
+app.get("/api/test", (req, res)=>{});
 
 app.get('/api/*', (req, res) => {
  res.json({ err: 404 });
