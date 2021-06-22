e90b93ee130c5aa21e6f5c1a9d0ee518045add41
sayantan300 <rovelstars300@gmail.com>
2021-06-22 21:41:50 +0530

    debug

--- src/server/mw/checkBanned.js
+++ src/server/mw/checkBanned.js
@@ -1,32 +1,36 @@
 module.exports = async function(req, res, next) {
  var user = res.locals.user;
+ console.log("one")
  if (user) {
-  if(typeof BannedList != "undefined"){
-  let list = BannedList;
-  let ban = list.map(user => user.user.id);
-  const Isbanned = (ban.includes(req.params.id)) ? true : false;
-  if (Isbanned) {
-   res.sendFile(path.resolve("src/public/assets/banned.html"));
-   fetch(`${process.env.DOMAIN}/api/client/log`, {
-    method: "POST",
-    headers: {
-     "content-type": "application/json"
-    },
-    body: JSON.stringify({
-     "secret": process.env.SECRET,
-     "title": `Banned User ${user.tag} tried to visit us!`,
-     "color": "#ff0000",
-     "desc": `**${user.tag}** (${user.id}) was banned before, and they tried to visit our site at path:\n\`${req.path}\``
+  if (typeof BannedList != "undefined") {
+   let list = BannedList;
+   console.log("two")
+   let ban = list.map(user => user.user.id);
+   const Isbanned = (ban.includes(req.params.id)) ? true : false;
+   if (Isbanned) {
+    res.sendFile(path.resolve("src/public/assets/banned.html"));
+    fetch(`${process.env.DOMAIN}/api/client/log`, {
+     method: "POST",
+     headers: {
+      "content-type": "application/json"
+     },
+     body: JSON.stringify({
+      "secret": process.env.SECRET,
+      "title": `Banned User ${user.tag} tried to visit us!`,
+      "color": "#ff0000",
+      "desc": `**${user.tag}** (${user.id}) was banned before, and they tried to visit our site at path:\n\`${req.path}\``
+     })
     })
-   })
-  }
+   }
   }
   else {
    console.log("[WARN] BannedList is not initialized. No user checkup will be done.");
-    next();
+   next();
+   console.log("three")
   }
  }
  else {
+  console.log("four")
   next();
  }
 };
