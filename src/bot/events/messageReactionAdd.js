d2d4d4fece6702f35f172bb5198ca3e4a8ef0fa3
sayantan300 <rovelstars300@gmail.com>
2021-06-27 08:02:18 +0530

    hmm

--- src/bot/events/messageReactionAdd.js
+++ src/bot/events/messageReactionAdd.js
@@ -1,8 +1,10 @@
 client.on("messageReactionAdd", function(reaction, user) {
+ if(reaction.message!==undefined){
  if (reaction.message.channel.id == "858200098612838430") {
   const id = reaction.message.content.replace("<@!","").replace(">","");
   if(id===user.id){
    reaction.message.delete();
   }
+ }
  }
 });
