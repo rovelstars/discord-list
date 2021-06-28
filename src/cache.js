fcf9b05c7df4a1fe742f5843866ea0e8e4d943fa
sayantan300 <rovelstars300@gmail.com>
2021-06-28 17:05:43 +0530

    hmm

--- src/cache.js
+++ src/cache.js
@@ -3,11 +3,12 @@ bots = require("@models/bots.js");
 users = require("@models/users.js");
 servers = require("@models/servers.js");
 globalThis.Cache = {};
+console.log("[CACHE] Started!");
 Cache.AllBots = await bots.find();
 Cache.AllUsers = await users.find();
 Cache.AllServers = await servers.find();
 Cache.models = {bots, users, servers};
-
+Cache.Bots = {};
 Cache.Bots.findOneById = function (q){
  return Bots[Bots.findIndex(b=>b.id==q)];
 }
@@ -16,7 +17,7 @@ Cache.Bots.findOneByCode = function (q){
  return Bots[Bots.findIndex(b=>b.code==q)];
 }
 
-Cache.clean = function(arg){
+Cache.Bots.clean = function(arg){
   const {_id, code, ...bot} = arg;
   return bot;
 }
