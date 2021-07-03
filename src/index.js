220a807f945e5f950dbe4becfeb9223f4e8d4b01
sayantan300 <rovelstars300@gmail.com>
2021-07-03 20:58:07 +0530

    um

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 require("module-alias/register");
+globalThis.WebSocket = require('isomorphic-ws')
+
 const v = process.version.slice(1,3);
 if(v<16){
  console.error("[ERROR] Node.js v16 or above is required.");
