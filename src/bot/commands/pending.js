584127c046b9426631480536a2d38aa8b4e2fbd7
sayantan300 <rovelstars300@gmail.com>
2021-07-12 10:22:43 +0530

    fix pending invites

--- src/bot/commands/pending.js
+++ src/bot/commands/pending.js
@@ -1,9 +1,9 @@
 Cache.models.bots.find({added: false}).limit(5).then(async bots=>{
- let msg="Showing the Oldest 5 Bots that are not added to this server:\n";
+ let msg="> Showing the Oldest 5 Bots that are not added to this server:\n";
  for await(const bot of bots){
   await fetch(`${process.env.DOMAIN}/api/client/mainserver/${bot.id}`).then(r=>r.json()).then(async d=>{
    if(!d.condition){
-    msg+= await `<https://discord.com/api/oauth2/authorize?client_id=${bot.id}&permissions=0&scope=bot> - ${bot.tag}\n`
+    msg+= await (bot.id.startsWith("1"))?`<${bot.invite}> - ${bot.tag} [⚠️ OLD BOT, CHANGE PERMS TO 0]\n`:`<https://discord.com/api/oauth2/authorize?client_id=${bot.id}&permissions=0&scope=bot> - ${bot.tag}\n`
    }
   });
  }
