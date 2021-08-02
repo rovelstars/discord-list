238e568a8232cfb9b5c5089e34ecce490eccdb65
NAME <rovelstars300@gmail.com>
2021-08-02 15:01:04 +0530

    remove jquery

--- src/public/assets/script.js
+++ src/public/assets/script.js
@@ -1,148 +1,170 @@
-$(window).load(function() {
- var emojiparse = function(body){ return twemoji.parse(body, emojiopts)};
- var theme = document.getElementById("meta-theme");
- if (theme) {
-  theme.style.display = "none";
-  const color = theme.innerText;
-  document.querySelector('meta[name="theme-color"]').setAttribute('content', color);
-  document.querySelector('meta[name="theme-color"]').setAttribute('msapplication-navbutton-color', color);
-  var nav = document.getElementsByClassName("navbar is-fixed-top");
-  nav[0].style.backgroundColor = color;
- }
- $("#object").fadeOut();
- $("#loading").fadeOut();
- var frames = document.getElementsByTagName('iframe');
- for (var frame of frames) {
-  frame.setAttribute("sandbox", "allow-forms allow-popups");
- }
- var online = true;
- const Toast = Swal.mixin({
-  toast: true,
-  position: 'top-end',
-  showConfirmButton: false,
-  timer: 3000,
-  timerProgressBar: true,
-  didOpen: (toast) => {
-   toast.addEventListener('mouseenter', Swal.stopTimer)
-   toast.addEventListener('mouseleave', Swal.resumeTimer)
+document.addEventListener("DOMContentLoaded", function () {
+  var emojiparse = function (body) {
+    return twemoji.parse(body, emojiopts);
+  };
+  var theme = document.getElementById("meta-theme");
+  if (theme) {
+    theme.style.display = "none";
+    const color = theme.innerText;
+    document
+      .querySelector('meta[name="theme-color"]')
+      .setAttribute("content", color);
+    document
+      .querySelector('meta[name="theme-color"]')
+      .setAttribute("msapplication-navbutton-color", color);
+    var nav = document.getElementsByClassName("navbar is-fixed-top");
+    nav[0].style.backgroundColor = color;
   }
- });
- window.addEventListener('error', function(event) {
-  Toast.fire({
-   icon: 'error',
-   title: 'Something went wrong!',
-   text: event.message
-  })
- });
- setInterval(() => {
-  if (navigator.onLine != online) {
-   if (navigator.onLine == false) {
-    Toast.fire({
-     icon: 'error',
-     title: 'No Network!'
-    });
-   }
-   if (navigator.onLine == true) {
-    Toast.fire({
-     icon: 'success',
-     title: 'Network Connection Back!'
-    });
-   }
-   online = navigator.onLine;
+  let object = document.getElementById("object");
+  object.style.transition = "0.8s";
+  object.style.opacity = "0";
+  let loading = document.getElementById("loading");
+  loading.style.transition = "0.8s";
+  loading.style.opacity = "0";
+  var frames = document.getElementsByTagName("iframe");
+  for (var frame of frames) {
+    frame.setAttribute("sandbox", "allow-forms allow-popups");
   }
- }, 1000);
+  var online = true;
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
+  window.addEventListener("error", function (event) {
+    Toast.fire({
+      icon: "error",
+      title: "Something went wrong!",
+      text: event.message,
+    });
+  });
+  setInterval(() => {
+    if (navigator.onLine != online) {
+      if (navigator.onLine == false) {
+        Toast.fire({
+          icon: "error",
+          title: "No Network!",
+        });
+      }
+      if (navigator.onLine == true) {
+        Toast.fire({
+          icon: "success",
+          title: "Network Connection Back!",
+        });
+      }
+      online = navigator.onLine;
+    }
+  }, 1000);
 
- if (typeof(Storage) !== undefined) {
-  if (localStorage.allowcookies != "yes") {
-   Swal.fire({
-    title: "Hey New Visitor!",
-    text: "Allow us to introduce RDL!",
-    imageUrl: "https://discord.rovelstars.com/assets/img/bot/logo.svg",
-    imageWidth: 100,
-    imageHeight: 100,
-    showCancelButton: false,
-    showDenyButton: true,
-    didRender: emojiparse,
-    confirmButtonText: '<span>Introduce to me!</span>',
-    denyButtonText: '<span>I know this site 😏</span>',
-    reverseButtons: true
-   }).then((result) => {
-    if (result.isConfirmed) {
-     Swal.fire({
-      title: "We are all-in-one!",
-      text: "Yeah, RDL is meant to be a list having everything from discord, from emojis, users, servers, templates & bots!",
-      confirmButtonText: "<span>That's awesome!</span>"
-     }).then(() => {
+  if (typeof Storage !== undefined) {
+    if (localStorage.allowcookies != "yes") {
       Swal.fire({
-       title: "We have currency!",
-       text: "Rovel Coins are used throughout the site and they are very important! But don't worry, RDL Discord Bot can provide you coins so you are ready to go!",
-       confirmButtonText: "<span>Wow!</span>"
-      }).then(() => {
-       Swal.fire({
-        title: "Login Fast!",
-        text: "Login if you want to contribute to RDL, or just change your preferences!",
-        confirmButtonText: "<span>Sure!</span>"
-       }).then(() => {
-        Swal.fire({
-         title: "We use cookies! 🍪",
-         text: "Cause they are tasty 😋!",
-         icon: "info",
-         didRender: emojiparse,
-         confirmButtonText: "<span>That's Awesome! 😎</span>"
-        })
-       })
-      })
-     }).then(() => {
-      localStorage.allowcookies = "yes";
-     })
+        title: "Hey New Visitor!",
+        text: "Allow us to introduce RDL!",
+        imageUrl: "https://discord.rovelstars.com/assets/img/bot/logo.svg",
+        imageWidth: 100,
+        imageHeight: 100,
+        showCancelButton: false,
+        showDenyButton: true,
+        didRender: emojiparse,
+        confirmButtonText: "<span>Introduce to me!</span>",
+        denyButtonText: "<span>I know this site 😏</span>",
+        reverseButtons: true,
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: "We are all-in-one!",
+            text: "Yeah, RDL is meant to be a list having everything from discord, from emojis, users, servers, templates & bots!",
+            confirmButtonText: "<span>That's awesome!</span>",
+          })
+            .then(() => {
+              Swal.fire({
+                title: "We have currency!",
+                text: "Rovel Coins are used throughout the site and they are very important! But don't worry, RDL Discord Bot can provide you coins so you are ready to go!",
+                confirmButtonText: "<span>Wow!</span>",
+              }).then(() => {
+                Swal.fire({
+                  title: "Login Fast!",
+                  text: "Login if you want to contribute to RDL, or just change your preferences!",
+                  confirmButtonText: "<span>Sure!</span>",
+                }).then(() => {
+                  Swal.fire({
+                    title: "We use cookies! 🍪",
+                    text: "Cause they are tasty 😋!",
+                    icon: "info",
+                    didRender: emojiparse,
+                    confirmButtonText: "<span>That's Awesome! 😎</span>",
+                  });
+                });
+              });
+            })
+            .then(() => {
+              localStorage.allowcookies = "yes";
+            });
+        } else {
+          Swal.fire({
+            title: "We use cookies! 🍪",
+            text: "Cause they are tasty 😋!",
+            icon: "info",
+            didRender: emojiparse,
+            confirmButtonText: "<span>That's Awesome! 😎</span>",
+          }).then(() => {
+            localStorage.allowcookies = "yes";
+          });
+        }
+      });
     }
-    else {
-     Swal.fire({
-      title: "We use cookies! 🍪",
-      text: "Cause they are tasty 😋!",
-      icon: "info",
-      didRender: emojiparse,
-      confirmButtonText: "<span>That's Awesome! 😎</span>"
-     }).then(() => { localStorage.allowcookies = "yes" })
-    }
-   })
+  } else {
+    Swal.fire({
+      title: "Uh Oh!",
+      text: "Your browser dosen't supports local storage! Please use another browser or update your browser!",
+      icon: "error",
+      confirmButtonText: "<span>RIP..</span>",
+    }).then(() => {
+      window.close();
+    });
   }
- } else {
-  Swal.fire({
-   title: "Uh Oh!",
-   text: "Your browser dosen't supports local storage! Please use another browser or update your browser!",
-   icon: "error",
-   confirmButtonText: "<span>RIP..</span>"
-  }).then(() => {
-   window.close();
-  })
- }
 });
 
-function onHover()
-{
-    $("#menuImg").attr('src', 'https://discord.rovelstars.com/assets/img/bot/pet.gif');
+function onHover() {
+  document.getElementById("menuImg").src =
+    "https://discord.rovelstars.com/assets/img/bot/pet.gif";
 }
 
-function offHover()
-{
-    $("#menuImg").attr('src', 'https://discord.rovelstars.com/assets/img/bot/logo.svg');
+function offHover() {
+  document.getElementById("menuImg").src =
+    "https://discord.rovelstars.com/assets/img/bot/logo.svg";
 }
 
- $(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function () {
   var logout = document.getElementById("logout-link");
-  if(logout){
-   logout.href=`/logout?redirect=${encodeURI(window.location)}`
+  if (logout) {
+    logout.href = `/logout?redirect=${encodeURI(window.location)}`;
   }
-twemoji.parse(document.body, emojiopts)
+  twemoji.parse(document.body, emojiopts);
   // Check for click events on the navbar burger icon
-  $(".navbar-burger").click(function() {
-
+  document
+    .getElementsByClassName("navbar-burger")
+    .addEventListener("click", function () {
       // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
-  });
-  $(".has-dropdown").click(function(){
-   $(".navbar-dropdown").toggleClass("is-hidden-touch");
-  });
+      document
+        .getElementsByClassName("navbar-burger")
+        .classList.toggle("is-active");
+      document
+        .getElementsByClassName("navbar-menu")
+        .classList.toggle("is-active");
+    });
+  document
+    .getElementsByClassName("navbar-dropdown")
+    .addEventListener("click", function () {
+      document
+        .getElementsByClassName("navbar-dropdown")
+        .classList.toggle("is-hidden-touch");
+    });
 });
