95b0d9bd68d994975f3d0310abef0a83021f2325
NAME <rovelstars300@gmail.com>
2021-08-02 14:32:31 +0530

    build caching starts now

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [
+    {
+      name: "main",
+      script: "src/index.js",
+      watch: ["src", "package.json"],
+      // Delay between restart
+      kill_timeout: 10000,
+      watch_delay: 5000,
+      exec_mode: "cluster",
+      instances: "max",
+      ignore_watch: [
+        ".git",
+        "rdl",
+        "src/public/*",
+        "sdk.js",
+        "README.md",
+        "TODO.md",
+      ],
+      watch_options: {
+        followSymlinks: false,
+      },
+    },
+  ],
+};
