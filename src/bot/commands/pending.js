509264fbf0dea31ded7e0513308fc6d68af2b7f8
sayantan300 <rovelstars300@gmail.com>
2021-08-23 13:04:00 +0530

    hmm

--- src/bot/commands/pending.js
+++ src/bot/commands/pending.js
@@ -3,7 +3,7 @@ Cache.models.bots.find({added: false}).limit(5).then(async bots=>{
  for await(const bot of bots){
   await fetch(`${process.env.DOMAIN}/api/client/mainserver/${bot.id}`).then(r=>r.json()).then(async d=>{
    if(!d.condition){
-    msg+= await (bot.id.startsWith("1") || bot.id.startsWith("2"))?`<${bot.invite}&permissions=0> - ${bot.tag} [⚠️ OLD BOT]\n`:`<https://discord.com/api/oauth2/authorize?client_id=${bot.id}&permissions=0&scope=bot&guild_id=602906543356379156> - ${bot.tag}\n`
+    msg+= await (bot.id.startsWith("1") || bot.id.startsWith("2"))?`<${bot.invite}&permissions=0&guild_id=602906543356379156> - ${bot.tag} [⚠️ OLD BOT]\n`:`<https://discord.com/api/oauth2/authorize?client_id=${bot.id}&permissions=0&scope=bot&guild_id=602906543356379156> - ${bot.tag}\n`
    }
   });
  }
