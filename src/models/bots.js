3ca6141c0d8e52d091643786eb2633f353ad2533
NAME <rovelstars300@gmail.com>
2021-08-27 16:32:01 +0530

    fixes: added '?' which answers the bug

--- src/models/bots.js
+++ src/models/bots.js
@@ -1,76 +1,94 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
-const {fetch} = require("rovel.js");
-const Bots = new Schema({
- _id: {
-    default: () => new Date(),
-    type: Date
-  }, //added at
- id: {
-  type: String , 
-  unique: true,
-  required: true
-  }, //botId
-  card: {
-   img: String,
-   title: String,
-   msg: String
+const { fetch } = require("rovel.js");
+const Bots = new Schema(
+  {
+    _id: {
+      default: () => new Date(),
+      type: Date,
+    }, //added at
+    id: {
+      type: String,
+      unique: true,
+      required: true,
+    }, //botId
+    card: {
+      img: String,
+      title: String,
+      msg: String,
+    },
+    username: { type: String, required: true },
+    avatar: { type: String, required: true },
+    discriminator: { type: String, required: true },
+    status: String,
+    owners: [{ type: String }], //owners
+    lib: { type: String },
+    short: String, //short desc
+    desc: String, //description
+    prefix: String, //bot prefix
+    verified: { type: Boolean, default: false }, //verified bot or not
+    added: { type: Boolean, default: false },
+    code: { type: String },
+    webhook: String,
+    support: String, //support server id
+    bg: String, // background image link
+    github: String, //github link
+    website: String, //website link
+    donate: String, //donate account link
+    invite: String, // invite link
+    servers: { type: Number, default: 1 },
+    promoted: { type: Boolean, default: false },
+    votes: { type: Number, default: 0 },
+    badges: [{ type: String }],
+    slug: {
+      type: String,
+      default: function () {
+        return this.id;
+      },
+      unique: true,
+    },
   },
-  username: {type: String, required: true},
-  avatar: {type: String, required: true},
-  discriminator: {type: String, required: true},
-  status: String,
- owners: [{type: String}], //owners
- lib: {type: String},
- short: String, //short desc
- desc: String, //description
- prefix: String, //bot prefix
- verified: { type: Boolean, default: false }, //verified bot or not
- added: {type: Boolean, default: false},
- code: {type: String},
- webhook: String,
- support: String, //support server id
- bg: String, // background image link
- github: String, //github link
- website: String, //website link
- donate: String, //donate account link
- invite: String, // invite link
- servers: { type: Number, default: 1 },
- promoted: { type: Boolean, default: false },
- votes: { type: Number, default: 0 },
- badges: [{ type: String }],
- slug: {type: String, default: function(){return this.id}, unique: true},
-},{ versionKey: false, toJSON: { virtuals: true }, toObject: { virtuals: true }});
+  {
+    versionKey: false,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
-Bots.virtual('avatarURL').get(function(){
-  if((this.avatar=="1")||(this.avatar=="2")||(this.avatar=="3")||(this.avatar=="4")) return `https://cdn.discordapp.com/embed/avatars/${this.avatar}.png`;
- var ani=false;
- if(this.avatar.startsWith("a_")) ani=true;
- const aniurl=`https://cdn.discordapp.com/avatars/${this.id}/${this.avatar}.gif`;
- const nonurl=`https://cdn.discordapp.com/avatars/${this.id}/${this.avatar}.png`;
- const url = (ani)?aniurl:nonurl;
- return url;
+Bots.virtual("avatarURL").get(function () {
+  if (
+    this.avatar == "1" ||
+    this.avatar == "2" ||
+    this.avatar == "3" ||
+    this.avatar == "4"
+  )
+    return `https://cdn.discordapp.com/embed/avatars/${this.avatar}.png`;
+  var ani = false;
+  if (this.avatar?.startsWith("a_")) ani = true;
+  const aniurl = `https://cdn.discordapp.com/avatars/${this.id}/${this.avatar}.gif`;
+  const nonurl = `https://cdn.discordapp.com/avatars/${this.id}/${this.avatar}.png`;
+  const url = ani ? aniurl : nonurl;
+  return url;
 });
 
-Bots.virtual('tag').get(function(){
- return `${this.username}#${this.discriminator}`;
+Bots.virtual("tag").get(function () {
+  return `${this.username}#${this.discriminator}`;
 });
-Bots.virtual('vanity').get(function(){
- return `https://dscrdly.com/b/${this.slug}`;
+Bots.virtual("vanity").get(function () {
+  return `https://dscrdly.com/b/${this.slug}`;
 });
-Bots.virtual('mainowner').get(function(){
- return `${this.owners[0]}`
+Bots.virtual("mainowner").get(function () {
+  return `${this.owners[0]}`;
 });
-Bots.virtual('timestamp').get(function(){
- return `${~~(this._id / 1000)}`
-})
-Bots.index({'$**': 'text'});
+Bots.virtual("timestamp").get(function () {
+  return `${~~(this._id / 1000)}`;
+});
+Bots.index({ "$**": "text" });
 var bots;
-try{
-console.log("[DB] Compiling Schema into Model - Bots");
-bots = mongoose.model('Bots', Bots);
-}
-catch(e){
- bots = mongoose.model('Bots');
+try {
+  console.log("[DB] Compiling Schema into Model - Bots");
+  bots = mongoose.model("Bots", Bots);
+} catch (e) {
+  bots = mongoose.model("Bots");
 }
 module.exports = bots;
--- src/models/users.js
+++ src/models/users.js
@@ -1,44 +1,56 @@
-const mongoose = require('mongoose');
-  const { Schema } = mongoose;
-  const Users = new Schema({
-   id: {type: String, unique: true, required: true},
-   _id: {
-    default: () => new Date(),
-    type: Date
-  }, //added at
-  username: {type: String, required: true},
-  avatar: {type: String, required: true},
-  discriminator: {type: String, required: true},
-  email: {type: String},
-   bal: {type: Number, default: 10},
-   bio: {type: String, default: "The user doesn't have bio set!"},
-   badges: [{type: String}],
-   promoted: {type: Boolean, default: false},
-   address: {type: String},
-   lang: {type: String, default: "en"}
-  },{ versionKey: false, toJSON: { virtuals: true }, toObject: { virtuals: true }});
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const Users = new Schema(
+  {
+    id: { type: String, unique: true, required: true },
+    _id: {
+      default: () => new Date(),
+      type: Date,
+    }, //added at
+    username: { type: String, required: true },
+    avatar: { type: String, required: true },
+    discriminator: { type: String, required: true },
+    email: { type: String },
+    bal: { type: Number, default: 10 },
+    bio: { type: String, default: "The user doesn't have bio set!" },
+    badges: [{ type: String }],
+    promoted: { type: Boolean, default: false },
+    address: { type: String },
+    lang: { type: String, default: "en" },
+  },
+  {
+    versionKey: false,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
-Users.virtual('avatarURL').get(function(){
-  if((this.avatar=="1")||(this.avatar=="2")||(this.avatar=="3")||(this.avatar=="4")){ return `https://cdn.discordapp.com/embed/avatars/${this.avatar}.png`;}
-  else{
- var ani=false;
- if(this.avatar.startsWith("a_")) ani=true;
- const aniurl=`https://cdn.discordapp.com/avatars/${this.id}/${this.avatar}.gif`;
- const nonurl=`https://cdn.discordapp.com/avatars/${this.id}/${this.avatar}.png`;
- const url = (ani)?aniurl:nonurl;
- return url;
+Users.virtual("avatarURL").get(function () {
+  if (
+    this.avatar == "1" ||
+    this.avatar == "2" ||
+    this.avatar == "3" ||
+    this.avatar == "4"
+  ) {
+    return `https://cdn.discordapp.com/embed/avatars/${this.avatar}.png`;
+  } else {
+    var ani = false;
+    if (this.avatar?.startsWith("a_")) ani = true;
+    const aniurl = `https://cdn.discordapp.com/avatars/${this.id}/${this.avatar}.gif`;
+    const nonurl = `https://cdn.discordapp.com/avatars/${this.id}/${this.avatar}.png`;
+    const url = ani ? aniurl : nonurl;
+    return url;
   }
 });
-Users.virtual('tag').get(function(){
- return `${this.username}#${this.discriminator}`;
-})
+Users.virtual("tag").get(function () {
+  return `${this.username}#${this.discriminator}`;
+});
 
 var users;
-try{
-console.log("[DB] Compiling Schema into Model - Users");
-users = mongoose.model('Users', Users);
-}
-catch(e){
- users = mongoose.model('Users');
+try {
+  console.log("[DB] Compiling Schema into Model - Users");
+  users = mongoose.model("Users", Users);
+} catch (e) {
+  users = mongoose.model("Users");
 }
 module.exports = users;
