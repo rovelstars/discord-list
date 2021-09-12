0704bfceee259be68d788a3682777a761a321b24
sayantan300 <rovelstars300@gmail.com>
2021-09-12 15:38:54 +0530

    fix

--- src/server/routes/authclient.js
+++ src/server/routes/authclient.js
@@ -42,6 +42,7 @@ router.get("/", async (req, res) => {
      avatar: (user.avatarHash) ? user.avatarHash : (user.discriminator % 5)
     }).save(async (err, userr) => {
      if (err) return console.log(err);
+     Cache.Users.refresh();
      fetch(`${process.env.DOMAIN}/api/client/log`, {
       method: "POST",
       headers: {
