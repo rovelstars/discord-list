c7a15055189515781aa2ca743417db5d41943812
sayantan300 <rovelstars300@gmail.com>
2021-09-21 10:49:50 +0530

    rovelstars.com has been added here

--- src/server/mw/linkService.js
+++ src/server/mw/linkService.js
@@ -1,60 +1,59 @@
 let router = require("express").Router();
+router.all("*", (req, res, next) => {
+  if (req.hostname == "rovelstars.com") {
+    res.set("location", `https://discord.rovelstars.com${req.path}`);
+    res.status(301).send();
+  } else next();
+});
 
-router.get("/b/:slug/invite",(req, res, next)=>{
- if(req.hostname!="dscrdly.com"){
-  next();
- }
- else{
-  Cache.Bots.findOne({slug: req.params.slug}).then(bot=>{
-   if(!bot){
-    res.render("404.ejs", { path: req.originalUrl });
-   }
-   else{
-    res.redirect(bot.invite);
-   }
-  })
- }
-})
+router.get("/b/:slug/invite", (req, res, next) => {
+  if (req.hostname != "dscrdly.com") {
+    next();
+  } else {
+    Cache.Bots.findOne({ slug: req.params.slug }).then((bot) => {
+      if (!bot) {
+        res.render("404.ejs", { path: req.originalUrl });
+      } else {
+        res.redirect(bot.invite);
+      }
+    });
+  }
+});
 
-router.get("/b/:slug/vote",(req, res, next)=>{
- if(req.hostname!="dscrdly.com"){
-  next();
- }
- else{
-  Cache.Bots.findOne({slug: req.params.slug}).then(bot=>{
-   if(!bot){
-    res.render("404.ejs", { path: req.originalUrl });
-   }
-   else{
-    res.redirect(`${process.env.DOMAIN}/bots/${bot.id}/vote`);
-   }
-  })
- }
-})
+router.get("/b/:slug/vote", (req, res, next) => {
+  if (req.hostname != "dscrdly.com") {
+    next();
+  } else {
+    Cache.Bots.findOne({ slug: req.params.slug }).then((bot) => {
+      if (!bot) {
+        res.render("404.ejs", { path: req.originalUrl });
+      } else {
+        res.redirect(`${process.env.DOMAIN}/bots/${bot.id}/vote`);
+      }
+    });
+  }
+});
 
-router.get("/b/:slug",(req, res, next)=>{
- if(req.hostname!="dscrdly.com"){
-  next();
- }
- else{
-  Cache.Bots.findOne({slug: req.params.slug}).then(bot=>{
-   if(!bot){
-    res.render("404.ejs", { path: req.originalUrl });
-   }
-   else{
-    res.redirect(`${process.env.DOMAIN}/bots/${bot.id}`);
-   }
-  })
- }
-})
+router.get("/b/:slug", (req, res, next) => {
+  if (req.hostname != "dscrdly.com") {
+    next();
+  } else {
+    Cache.Bots.findOne({ slug: req.params.slug }).then((bot) => {
+      if (!bot) {
+        res.render("404.ejs", { path: req.originalUrl });
+      } else {
+        res.redirect(`${process.env.DOMAIN}/bots/${bot.id}`);
+      }
+    });
+  }
+});
 
-router.get("*",(req, res, next)=>{
- if(req.hostname=="dscrdly.com"){
-  res.redirect(`${process.env.DOMAIN}${req.originalUrl}`);
- }
- else{
-  next();
- }
-})
+router.get("*", (req, res, next) => {
+  if (req.hostname == "dscrdly.com") {
+    res.redirect(`${process.env.DOMAIN}${req.originalUrl}`);
+  } else {
+    next();
+  }
+});
 
 module.exports = router;
