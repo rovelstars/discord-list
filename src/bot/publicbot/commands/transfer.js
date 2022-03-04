c86d7e8a23ad981df87abe85ba7b9fca4b6f8418
sayantan.das.gg@gmail.com <sayantan.das.gg@gmail.com>
2022-03-04 16:28:16 +0530

    fixes, transferring back to you shouldn't clear away your money!

--- src/bot/publicbot/commands/transfer.js
+++ src/bot/publicbot/commands/transfer.js
@@ -19,11 +19,13 @@ message.reply({content: "You forgot to ping or write the ID of the user to whom 
         message.reply({content: "You need to specify"})
       }
       if(args[1]=="--all"){
-        user.bal+=uu.bal;
+        args[1]=uu.bal;
         uu.bal=0;
+        user.bal+=args[1];
+        //this fixes the bug where transferring back the R$ doesn't turns the amount to zero!
         user.save();
         setTimeout(()=>{uu.save();},100); //this fixes the wierd problem with parallel save error.
-        message.reply({content: `Sent <:Rcoin:948896802298548224> ${uu.bal} to ${user.tag}!\n${user.username}'s Balance: <:Rcoin:948896802298548224> ${user.bal}\nYour balance is <:Rcoin:948896802298548224> 0! Go and start earning some <:Rcoin:948896802298548224>!`});
+        message.reply({content: `Sent <:Rcoin:948896802298548224> ${uu.bal} to ${user.tag}!\n${user.username}'s Balance: <:Rcoin:948896802298548224> ${user.bal}\nYour balance is <:Rcoin:948896802298548224> ${uu.bal +((uu.id==user.id)?" (It should be 0 right? Well idk what happened!)":"!Go and start earning some <:Rcoin:948896802298548224>!")}`});
       }
       if(!isNaN(args[1])){
         if(parseInt(args[1])>uu.bal){
