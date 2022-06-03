d873a53cda870e201eeb6475b20bab4bb9a4565c
Ren Hiyama <92968534+renhiyama@users.noreply.github.com>
2022-06-03 10:37:45 +0530

    dont ping devs for some time :D

--- src/bot/events/guildMemberRemove.js
+++ src/bot/events/guildMemberRemove.js
@@ -31,6 +31,7 @@ client.on("guildMemberRemove", (member) => {
         }
       }
     }
+    /*
     if (member.user.bot) {
       Cache.Bots.findOne({ id: member.user.id }).then((bot) => {
         if (!bot) return;
@@ -57,6 +58,6 @@ client.on("guildMemberRemove", (member) => {
           }
         }
       });
-    }
+    }*/
   }
 });
